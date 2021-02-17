7ff8e87f9a04f9972c41b75d79117e8308fbd9a2
enoz12 <enoz12@nate.com>
2021-02-17 23:23:21 +0900

    Updates

---
+++ static/js/main.5534cd13.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptemplate=this.webpackJsonptemplate||[]).push([[0],{10:function(t,e,n){},11:function(t,e,n){"use strict";n.r(e);var c=n(0),r=n(1),o=n.n(r),i=n(3),s=n.n(i);n(9),n(10);var a=function(){return Object(c.jsx)("h1",{children:"hello :)"})};s.a.render(Object(c.jsx)(o.a.StrictMode,{children:Object(c.jsx)(a,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.5534cd13.chunk.js.map
